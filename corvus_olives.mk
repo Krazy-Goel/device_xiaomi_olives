37ba42aded134e1d4d3a696dc1bf8b632bda48f4
Krazy-Goel <88996044+Krazy-Goel@users.noreply.github.com>
2022-02-17 17:48:18 +0530

    Update corvus_olives.mk

--- corvus_olives.mk
+++ corvus_olives.mk
@@ -9,33 +9,39 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
+# Inherit some common Komodo OS stuff
+$(call inherit-product, vendor/corvus/config/common.mk)
 
 # Inherit from olives device
 $(call inherit-product, device/xiaomi/olives/device.mk)
 
-# Corvus Stuff
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := olive
+PRODUCT_NAME := corvus_olive
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 8
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+
+# Komodo Stuff
 CORVUS_OFFICIAL := true
 USE_GAPPS := true
+CORVUS_VARIANT := RELEASE
 
+# Some build flags
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_FACE_UNLOCK := true
 TARGET_USES_BLUR := true
 TARGET_USES_AOSP_CHARGER := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
-IS_PHONE := true
-
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := olives
-PRODUCT_NAME := corvus_olives
-BOARD_VENDOR := Xiaomi
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi 8 Series
-PRODUCT_MANUFACTURER := Xiaomi
-TARGET_VENDOR := Xiaomi
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ARCORE := false
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220105.002 7961164 release-keys"
+
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNCNXM:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys"
