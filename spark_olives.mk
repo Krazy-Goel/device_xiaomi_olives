e60afa509b8457e6c3ab9cdd7f6cf5c73779d6e2
Krazy-Goel <88996044+Krazy-Goel@users.noreply.github.com>
2022-02-23 16:52:16 +0530

    Update spark_olives.mk

--- spark_olives.mk
+++ spark_olives.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Komodo OS stuff
-$(call inherit-product, vendor/spark/config/common.mk)
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
 # Inherit from olives device
 $(call inherit-product, device/xiaomi/olives/device.mk)
